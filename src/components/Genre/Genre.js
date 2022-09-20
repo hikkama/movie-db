f604e630b895af4d1422d7a3a1c9369caefe9e1e
hikkama <khasanov.nurlan@gmail.com>
2022-09-20 19:37:27 +0500

    Change Genre component

--- src/components/Genre/Genre.js
+++ src/components/Genre/Genre.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
 import { Tag } from 'antd'
 
 import Context from '../MovieDbContext'
@@ -8,17 +9,20 @@ import styles from './Genre.module.css'
 function Genre({ genreIds }) {
   const { genres } = useContext(Context)
 
-  return genreIds.map((id) => {
-    const genre = genres.find((item) => item.id === id)
-    if (!genre) {
-      return null
-    }
-    return (
-      <Tag className={styles.cardTag} key={id}>
-        {genre.name}
-      </Tag>
-    )
-  })
+  const filtered = genreIds.map((id) => genres.find((item) => item.id === id))
+  if (!filtered) {
+    return null
+  }
+
+  return filtered.map((tag) => (
+    <Tag className={styles.cardTag} key={tag.id}>
+      {tag.name}
+    </Tag>
+  ))
+}
+
+Genre.propTypes = {
+  genreIds: PropTypes.arrayOf(PropTypes.number).isRequired,
 }
 
 export default Genre
